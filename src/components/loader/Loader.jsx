531a9ac1290be1e1c80d9f22d9b5dd4c2a0018e6
mg5566 <mg5566@hanmail.net>
2023-08-28 06:21:23 +0000

    [feat] create loader component

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React from 'react';
+import styles from './Loader.module.scss';
+import { RotatingLines } from 'react-loader-spinner';
 
-const Loader = () => {
-  return <div>Loader</div>;
+/**
+ * Loading Spinner
+ *
+ * @todo
+ * 현재 z-index 를 사용하고 있습니다.
+ * 향후, react potal 기능으로 변경해야합니다.
+ *
+ */
+const Loader = ({ basic }) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
