0d3869362cee530aed4422b962640658c4d70eab
mg5566 <mg5566@hanmail.net>
2023-09-05 05:55:38 +0000

    [feat] apply firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { getFireStore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5j8sXNDTlrdgSfQakLjzoPI-xqXT8dTU",
+  authDomain: "next-shop-app-e687c.firebaseapp.com",
+  projectId: "next-shop-app-e687c",
+  storageBucket: "next-shop-app-e687c.appspot.com",
+  messagingSenderId: "767777273846",
+  appId: "1:767777273846:web:a66d276f331c7329c679d7",
+  measurementId: "G-Z4GCNP9BH7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFireStore(app);
+export const store = getStore(app);
